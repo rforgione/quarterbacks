5e39e369d6671d96d81902b8411a231cec7b98a8
Robert Forgione <rforgione@gmail.com>
2014-10-06 21:24:36 -0400

    Updated BeautifulSoup call to use HTML5 parser, and added URL into scrub_data docstring

--- draft_scrub.py
+++ draft_scrub.py
@@ -5,19 +5,19 @@ import numpy as np
 
 def scrub_data(url):
 	""" Takes NFL combine data from www.nflcombineresults.com and turns it into a 
-	pandas dataframe. """
+	pandas dataframe. The script was built with the following URL in mind:
+	http://nflcombineresults.com/nflcombinedata.php?year=all&pos=&college= """
 	
 	url = urllib2.urlopen(url)
 	page = url.read()
-	soup = BeautifulSoup(page)
+	soup = BeautifulSoup(page, 'html5lib')
 
-	elems = [i.text for i in soup.tbody.find_all("div", {'align': 'center'})]
-
-	dataset = []
+	elems = [i.text for i in soup.tbody.find_all('div', {'align': 'center'})]
 
 	vec = np.array(elems)
 
 	mat = vec.reshape(-1, 13)
 
 	return pd.DataFrame(mat, columns=['Year', 'Name', 'College', 'POS', 'Height.in', 'Weight.lbs', 
-									  'Wonderlic', '40yd', 'Bench', 'Vert', 'BroadJump', 'Shuttle', '3cone'])
+			   					      'Wonderlic', '40yd', 'Bench', 'Vert', 'BroadJump', 'Shuttle', '3cone'])
+
